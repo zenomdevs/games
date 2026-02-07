ab84b9ee6f2be7ebf5d10f36fdd4fb480bd71a45
𓆰𖤝 𝐙𝐞𝐧𝐨 𝐃𝐞𝐱𝐱 𖤝𓆪 <yatocriador@gmail.com>
2026-02-07 16:31:13 -0300

    Update target position in aura.lua

--- aura.lua
+++ aura.lua
@@ -6,7 +6,7 @@ local Lighting = game:GetService("Lighting")
 local player = Players.LocalPlayer
 
 -- Target position
-local TARGET_CFRAME = CFrame.new(3890, 210.46, 39.72)
+local TARGET_CFRAME = CFrame.new(4290, 210.46, 39.72)
 
 -- State
 local autoTeleport = false
